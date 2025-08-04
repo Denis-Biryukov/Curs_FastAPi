9dab5f71c314771567dd4433dc9f937fd822ce7e
Starley <den.biryukov.2021@gmail.com>
2025-08-04 23:20:15 +0300

    pagination

--- hotels.py
+++ hotels.py
@@ -1,2 +1,112 @@
-if __name__ == '__main__': 
-    main()
+from fastapi import Query, Body, APIRouter
+from pydantic import BaseModel
+
+
+router = APIRouter(prefix="/hotels")
+
+class Hotel(BaseModel):
+    title: str
+    description: str
+
+
+hotels = [
+    {"id": 1, "title": "Москва", "description": "Hotel in Moscow"},
+    {"id": 2, "title": "Лондон", "description": "Hotel in London"},
+    {"id": 3, "title": "Париж", "description": "Hotel in Paris"},
+    {"id": 4, "title": "Dubai", "description": "Hotel in Dubai"},
+    {"id": 5, "title": "Мальдивы", "description": "Hotel in Maldivi"},
+    {"id": 6, "title": "Kazan", "description": "Hotel in Kazan"},
+    {"id": 7, "title": "New York", "description": "Hotel in New York"},
+    {"id": 8, "title": "Saransk", "description": "Hotel in Saransk"}
+]
+
+
+@router.get("/", summary="Показать отели")
+async def get_hotels(
+        id: int | None = Query(None, description="id"),
+        title: str | None = Query(None, description="Название отеля"),
+        page: int  | None = Query(1, description="Номер страницы"),
+        per_page: int | None = Query(3, description="Количество на странице"),
+):
+    # global hotels
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    # return [hotel for hotels_ [per_page: page]]
+    return hotels_[((page - 1) * per_page):(per_page * page)]
+
+
+@router.post("", summary="Добавить отель")
+async def creat_hotel(data: Hotel
+        # title: str = Body(),
+        # description: str = Body(),
+):
+    global hotels
+    hotels.append(
+        {
+            "id": hotels[-1]["id"] + 1,
+            "title": data.title,
+            "description": data.description,
+        }
+    )
+    return {"status": "Ok"}
+
+
+@router.put("/{hotel_id}", summary="Изменить отель")
+async def update_all_hotel(
+        hotel_id: int,
+        title: str = Body(),
+        description: str = Body(),
+        ):
+    global hotels
+
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    hotel["title"] = title
+    hotel["description"] = description
+
+    # for hotel in hotels:
+    #     if hotel_id and hotel["id"] == hotel_id:
+    #         hotel["title"] = title
+    #
+    #         hotel["description"] = description
+    return {"update": "Ok", "hotels": hotels}
+
+
+@router.patch("/{hotel_id}", summary="Внести изменения в отель")
+async def update_hotel(
+        hotel_id: int,
+        title: str | None = Body(default=None),
+        description: str | None = Body(default=None),
+):
+    global hotels
+
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    if title:
+        hotel["title"] = title
+    if description:
+        hotel["description"] = description
+    return { "hotel": hotel, "update": "Ok"}
+    # for hotel in hotels:
+    #     if hotel_id and hotel["id"] == hotel_id and title != "string" and description != "string":
+    #         hotel["title"] = title
+    #         hotel["description"] = description
+    #     elif hotel_id and hotel["id"] == hotel_id and title != "string":
+    #         hotel["title"] = title
+    #     elif hotel_id and hotel["id"] == hotel_id and description != "string":
+    #         hotel["description"] = description
+    #     else:
+    #         return {"update": False, "message": "Изменений нет"}
+    #
+    #     return {"update": "Ok", "hotels": hotels}
+
+
+
+@router.delete("/{hotel_id}", summary="Удалить отель")
+async def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "Ok"}
--- main.py
+++ main.py
@@ -1,123 +1,37 @@
-from fastapi import FastAPI, Query, Body
+from fastapi import FastAPI
 from fastapi.openapi.docs import (
     get_redoc_html,
-    get_swagger_ui_html,
-    get_swagger_ui_oauth2_redirect_html)
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-
+    get_swagger_ui_html,)
 
 import uvicorn
 
+from test import router as test_router
+from hotels import router as hotels_router
+
+
+
 
 app = FastAPI()
-
-
-
-@app.get("/docs")
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - Swagger UI",
-        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="https://unpq.com/swagger-ui-dast@5/swagger-ui-boudle.js",
-    )
+app.include_router(test_router, tags=["Тесты"])
+app.include_router(hotels_router, tags=["Отели"])
 
 
 
 
-hotels = [
-    {"id": 1, "title": "Москва", "description": "Hotel in Moscow"},
-    {"id": 2, "title": "Лондон", "description": "Hotel in London"},
-    {"id": 3, "title": "Париж", "description": "Hotel in Paris"},
-]
-
-
-@app.get("/hotels", summary="Показать отели")
-async def get_hotels(
-        id: int | None = Query(None, description="id"),
-        title: str | None = Query(None, description="Название отеля")
-):
-    # global hotels
-    hotels_ = []
-    for hotel in hotels:
-        if id and hotel["id"] != id:
-            continue
-        if title and hotel["title"] != title:
-            continue
-        hotels_.append(hotel)
-    return hotels_
-
-@app.post("/hotels", summary="Добавить отель")
-async def creat_hotel(
-        title: str = Body(embed=True),
-        description: str = Body(embed=True),
-):
-    global hotels
-    hotels.append(
-        {
-            "id": hotels[-1]["id"] + 1,
-            "title": title,
-            "description": description,
-        }
-    )
-    return {"status": "Ok"}
-
-
-@app.put("/hotels/{hotel_id}", summary="Изменить отель")
-async def update_all_hotel(
-        hotel_id: int,
-        title: str = Body(),
-        description: str = Body(),
-        ):
-    global hotels
-
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    hotel["title"] = title
-    hotel["description"] = description
-
-    # for hotel in hotels:
-    #     if hotel_id and hotel["id"] == hotel_id:
-    #         hotel["title"] = title
-    #
-    #         hotel["description"] = description
-    return {"update": "Ok", "hotels": hotels}
-
-
-@app.patch("/hotels/{hotel_id}", summary="Внести изменения в отель")
-async def update_hotel(
-        hotel_id: int,
-        title: str | None = Body(default=None),
-        description: str | None = Body(default=None),
-):
-    global hotels
-
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    if title:
-        hotel["title"] = title
-    if description:
-        hotel["description"] = description
-    return { "hotel": hotel, "update": "Ok"}
-    # for hotel in hotels:
-    #     if hotel_id and hotel["id"] == hotel_id and title != "string" and description != "string":
-    #         hotel["title"] = title
-    #         hotel["description"] = description
-    #     elif hotel_id and hotel["id"] == hotel_id and title != "string":
-    #         hotel["title"] = title
-    #     elif hotel_id and hotel["id"] == hotel_id and description != "string":
-    #         hotel["description"] = description
-    #     else:
-    #         return {"update": False, "message": "Изменений нет"}
-    #
-    #     return {"update": "Ok", "hotels": hotels}
 
 
 
-@app.delete("/hotels/{hotel_id}", summary="Удалить отель")
-async def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
-    return {"status": "Ok"}
+# @app.get("/docs", tags=["docs"])
+# async def custom_swagger_ui_html():
+#     return get_swagger_ui_html(
+#         # openapi_url=app.openapi_url,
+#         title=app.title + " - Swagger UI",
+#         # oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+#         swagger_js_url="https://unpq.com/swagger-ui-dast@5/swagger-ui-boudle.js",
+#     )
+
+
+
 
 
 
